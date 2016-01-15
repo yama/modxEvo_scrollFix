2110d4a877b72161951e71280f4cb98634df655c
Deesen <dh@fuseit.de>
2016-01-15 17:32:53 +0100

    v0.4

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # modxEvo_scrollFix
-Remembers last scroll-position after pressing Save-button, or leaving a page in Modx Evolution Manager  
+Offers several features to optimize workflow within Modx Evolution Manager:
+
+- "ScrollFix": Remembers last scroll-position after pressing Save-button, or leaving a page (for Modx 1.0)
+- "Fix Tab-Header": Moves Scrollbar from Window to Tab-Pane so Tab-Headers are always within viewport and accessible. Also remembers last Scroll-Position if ScrollFix is enabled! 
+- "Add ToTop-Button": Scrolls to window-top, or tab-header top if fixed. Can be styled via custom CSS
+- "Extend Save-Button": Splits Save-Button + Dropdown into 3 separate Save-Buttons
+- "Load jQuery from CDN": In case jQuery can not be found, this option acts as fallback to load jQuery from code.jquery.com
 
 #### Tested with
-- Modx Evolution 1.0.9 - 1.0.15
-- ManagerManager 0.6.2
+- Modx Evolution 1.0.5 - 1.1
+- ManagerManager 0.6.2 (mm_createTab, mm_moveFieldsToTab)
 - MultiTV 2.0
 
 #### Version History
+##### v0.4 - *2016-01-15*
+- New Feature "Extend Save-Button"
+- New Feature "ToTop-Button Custom": Custom CSS and label to change default styling
+- Compatible with new Modx 1.1 internal ScrollFix-Feature: Plugin´s ScrollFix without "Fix Tab-Header" enabled gets automatically disabled in Modx 1.1 for now
+
 ##### v0.3 - *2016-01-10*
-- New Feature "Fix Tab-Header": Moves Scrollbar from Window to Tab-Pane so Tab-Headers are always within viewport and accessible. Also remembers last Scroll-Position if ScrollFix is enabled! 
+- New Feature "Fix Tab-Header"
 - Added: Alert-Message if jQuery is not loaded
 - Added: Optionally load jQuery from code.jquery.com
 - Optimized Code
--- assets/plugins/scrollfix/plugin.scrollfix.php
+++ assets/plugins/scrollfix/plugin.scrollfix.php
@@ -6,63 +6,72 @@
  * ScrollFix:  http://forums.modx.com/thread/92462/remember-position-of-resource-page-after-saving-in-evo
  * AlwaysStay: https://github.com/extras-evolution/AlwaysStay/blob/master/install/assets/plugins/AlwaysStay.tpl
  *
+ * Latest version on Github: https://github.com/Deesen/modxEvo_scrollFix/
+ *
  * @category    plugin
- * @version     0.3
- * @date        10.01.2016
+ * @version     0.4
+ * @date        15.01.2016
  * @author      dh@fuseit.de
  * @internal    @configuration:
- *              &jQueryCdn=Load jQuery from CDN (code.jquery.com);list;Enabled,Disabled;Disabled &alwaysStay=AlwaysStay;list;Enabled,Disabled;Disabled &scrollFix=ScrollFix;list;Enabled,Disabled;Disabled &fixTabHeader=Fix Tab-Header;list;Enabled,Disabled;Disabled &cookieLifetime=ScrollFix Cookie-Lifetime (Minutes);int;120 &addToTopButton=Add ToTop-Button;list;Enabled,Disabled;Disabled
+ *              &scrollFix=ScrollFix;list;Enabled,Disabled;Disabled &fixTabHeader=Fix Tab-Header;list;Enabled,Disabled;Disabled &cookieLifetime=ScrollFix Cookie-Lifetime (Minutes);int;120 &addToTopButton=Add ToTop-Button;list;Enabled,Disabled;Disabled &addToTopCustom=ToTop-Button Custom-CSS (replace semicolon by |);textarea;bottom:15px|right:40px|width:40px|height:40px|line-height:40px|font-size:20px|background-color:#89AD4A|color:#fff|border:1px solid #658F1A|border-radius:5px &addToTopLabel=ToTop-Button Custom-Label;text &extSaveButtons=Extend Save-Button;list;Enabled,Disabled;Disabled &alwaysStay=AlwaysStay;list;Enabled,Disabled;Disabled &jQueryCdn=Load jQuery from CDN (code.jquery.com);list;Enabled,Disabled;Disabled
  * @internal    @events:
  *              OnManagerMainFrameHeaderHTMLBlock
- */
+ **/
 
-$jQueryCdn      = isset( $jQueryCdn ) ? $jQueryCdn : 'Disabled';
-$alwaysStay     = isset( $alwaysStay ) ? $alwaysStay : 'Disabled';
 $scrollFix      = isset( $scrollFix ) ? $scrollFix : 'Disabled';
+$fixTabHeader	= isset( $fixTabHeader ) ? $fixTabHeader : 'Disabled';
 $cookieLifetime = isset( $cookieLifetime ) ? $cookieLifetime : 120;
 $addToTopButton = isset( $addToTopButton ) ? $addToTopButton : 'Disabled';
-$fixTabHeader	= isset( $fixTabHeader ) ? $fixTabHeader : 'Disabled';
+$addToTopCustom = isset( $addToTopCustom ) ? $addToTopCustom : 'bottom:15px|right:40px|width:40px|height:40px|line-height:40px|font-size:20px|background-color:#89AD4A|color:#fff|border:1px solid #658F1A|border-radius:5px';
+$addToTopLabel  = isset( $addToTopLabel ) ? $addToTopLabel : '&#x25B2;';
+$extSaveButtons	= isset( $extSaveButtons ) ? $extSaveButtons : 'Disabled';
+$alwaysStay     = isset( $alwaysStay ) ? $alwaysStay : 'Disabled';
+$jQueryCdn      = isset( $jQueryCdn ) ? $jQueryCdn : 'Disabled';
 
 $e = & $modx->Event;
 
 if ( $e->name == "OnManagerMainFrameHeaderHTMLBlock" ) {
 
-	$jDocReady = array();
-	$jWinLoad = array();
-	$jWinUnLoad = array();
-	$jWinResize = array();
+    $jDocReady = array();
+    $jWinLoad = array();
+    $jWinUnLoad = array();
+    $jWinResize = array();
 
-	// PREPARE HTML-OUTPUT
-	$html = '<!-- ScrollFix Start -->';
+    // DETERMINE MODX v1.1
+    $version = $modx->getVersionData();
+    $modx11 = substr($version['version'],0,3) == '1.1';
 
-	// TAKE CARE OF JQUERY
-	if( $scrollFix != 'Disabled' || $alwaysStay != 'Disabled' || $addToTopButton != 'Disabled' || $fixTabHeader != 'Disabled') {
-		// ADD JQUERY FROM LOCAL
-		$html .= '
+    // PREPARE HTML-OUTPUT
+    $html = '<!-- ScrollFix Start -->';
+
+    // TAKE CARE OF JQUERY
+    if( $scrollFix != 'Disabled' || $alwaysStay != 'Disabled' || $addToTopButton != 'Disabled' || $fixTabHeader != 'Disabled') {
+	// ADD JQUERY FROM LOCAL
+	$html .= '
 		<script>!window.jQuery && document.write(unescape(\'%3Cscript src="/assets/js/jquery.min.js"%3E%3C/script%3E\'))</script>';
 
-		// ADD JQUERY FROM CDN
-		if( $jQueryCdn != 'Disabled' ) {
-		    $html .= '
+	// ADD JQUERY FROM CDN
+	if( $jQueryCdn != 'Disabled' ) {
+	    $html .= '
 		<script>!window.jQuery && document.write(unescape(\'%3Cscript src="https://code.jquery.com/jquery.min.js"%3E%3C/script%3E\'))</script>';
-		};
+	};
 
-		// ADD ERROR-MSG & NO-CONFLICT
-		$html .= '
+	// ADD ERROR-MSG & NO-CONFLICT
+	$html .= '
 		<script>!window.jQuery && alert(\'ScrollFix: jQuery not found! Enable "Load jQuery from CDN" in Plugin-Configuration\');
 				if( window.jQuery ) window.$j = jQuery.noConflict();
 		</script>';
-	};
+    };
 
-	// PREPARE SCROLLFIX
-	$scrollFixFunctions = '';
-	if( $scrollFix != 'Disabled' ) {
+    // PREPARE SCROLLFIX
+    $scrollFixFunctions = '';
+    if( $scrollFix != 'Disabled' ) {
 
-		// PREPARE GET- / SET-SCROLL FUNCTIONS
-		if( $fixTabHeader != 'Disabled' ) {
+	// PREPARE GET- / SET-SCROLL FUNCTIONS
+	if ($fixTabHeader != 'Disabled') {
 
-			// GET POSITION OF TAB-CONTAINER
-			$getSetScrollFunctions = '
+	    // GET POSITION OF TAB-CONTAINER
+	    $getSetScrollFunctions = '
 			function getScrollXY() {
 				var x = 0, y = 0;
 				var t = $j(".tab-pane").find(".tab-page:visible:first");
@@ -77,10 +86,10 @@ if ( $e->name == "OnManagerMainFrameHeaderHTMLBlock" ) {
 				t.scrollTop(y);
 			}';
 
-		} else {
+	} else {
 
-			// GET POSITION OF WINDOW
-			$getSetScrollFunctions = '
+	    // GET POSITION OF WINDOW
+	    $getSetScrollFunctions = '
 			function getScrollXY() {
 				var x = 0, y = 0;
 				if( typeof( window.pageYOffset ) == "number" ) {
@@ -102,9 +111,10 @@ if ( $e->name == "OnManagerMainFrameHeaderHTMLBlock" ) {
 			function setScrollXY(x, y) {
 				window.scrollTo(x, y);
 			}';
-		};
+	};
 
-		$scrollFixFunctions = $getSetScrollFunctions .'
+	if (!$modx11 || $fixTabHeader != 'Disabled') {
+	    $scrollFixFunctions = $getSetScrollFunctions . '
 
 			function createCookie(name,value,minutes) {
 				if (minutes) {
@@ -135,8 +145,8 @@ if ( $e->name == "OnManagerMainFrameHeaderHTMLBlock" ) {
 
 			function unloadP(pageref){
 				s=getScrollXY()
-				createCookie(pageref+"x",s[0],'. $cookieLifetime .');
-				createCookie(pageref+"y",s[1],'. $cookieLifetime .');
+				createCookie(pageref+"x",s[0],' . $cookieLifetime . ');
+				createCookie(pageref+"y",s[1],' . $cookieLifetime . ');
 			}
 
 			// GENERATE pageref AS COOKIE-ID
@@ -146,62 +156,86 @@ if ( $e->name == "OnManagerMainFrameHeaderHTMLBlock" ) {
 			if(params["id"] != undefined) { pageref += "_id"+params["id"]; }
 		';
 
-		$jWinLoad[] = '$j("#Button1 > a").removeAttr("href").css("cursor","pointer");';
-		$jWinLoad[] = 'loadP(pageref);';
-		$jWinUnLoad[] = 'unloadP(pageref);';
-
+	    $jWinLoad[] = 'loadP(pageref);';
+	    $jWinUnLoad[] = 'unloadP(pageref);';
 	};
+    };
 
-	// PREPARE ALWAYS-STAY
-	if( $alwaysStay != 'Disabled' ) {
-		$jDocReady[] = 'if($j("#stay")) { if(!$j("#stay").val()) { $j("#stay").val(2); }};';
-	};
+    // PREPARE ALWAYS-STAY
+    if( $alwaysStay != 'Disabled' && $extSaveButtons == 'Disabled' ) {
+	$jDocReady[] = 'if($j("#stay")) { if(!$j("#stay").val()) { $j("#stay").val(2); }};';
+    };
 
-	// PREPARE FIX TAB-HEADER $tabHeight
-	$tabHeightFunctions = '';
-	if( $fixTabHeader != 'Disabled' ) {
-		$tabHeightFunctions = '
+    // PREPARE FIX TAB-HEADER $tabHeight
+    $tabHeightFunctions = '';
+    if( $fixTabHeader != 'Disabled' ) {
+	$tabHeightFunctions = '
 			function setTabPageHeight() {
+				$j("html").css("overflow","hidden");
 				$j(".tab-page").css("box-sizing","content-box");
+				$j(".sectionBody > p:first").html("");
 				var winHeight = $j(window).height();
 				var tabsHeight = $j("h2.tab:first").height();
 				var tabOffset = $j(".tab-pane:first").offset();
 				var height = winHeight - tabsHeight - tabOffset.top * 2;
-				$j(".tab-page").css("overflow","auto").css("height",height+"px");
+				$j(".tab-page").css("overflow","auto").css("max-height",height+"px");
 			};
 		';
-		$jDocReady[] = '$j(".sectionBody").css("margin-bottom","0");';
-		$jDocReady[] = 'setTabPageHeight();';
-		$jDocReady[] = 'setTimeout(setTabPageHeight, 100);';
-		$jWinResize[] = 'setTabPageHeight();';
-	};
+	$jDocReady[] = '$j(".sectionBody").css("margin-bottom","0");';
+	$jDocReady[] = 'setTabPageHeight();';
+	$jDocReady[] = 'setTimeout(setTabPageHeight, 100);';
+	$jWinResize[] = 'setTabPageHeight();';
+    };
 
-	// PREPARE ScrollToTop-BUTTON
-	if( $addToTopButton != 'Disabled' ) {
-		$jWinLoad[] = '$j("body").append("<a onclick=\"setScrollXY(0,0)\" style=\"position:fixed;z-index:999999;bottom:10px;right:35px;display:block;width:50px;height:50px;line-height:50px;font-size:30px;text-align:center;background-color:#89AD4A;color:#fff;border:1px solid #658F1A;border-radius:5px;cursor:pointer;text-decoration:none;\">&#x25B2;</a>");';
-	};
+    // PREPARE ScrollToTop-BUTTON
+    if( $addToTopButton != 'Disabled' ) {
+	$toTopFunc = ( $modx11 && $fixTabHeader == 'Disabled') ? 'window.scrollTo(0,70)' : 'setScrollXY(0,0)';
+	$addToTopCustom = str_replace('|',';',$addToTopCustom);
+	$jWinLoad[] = '$j("body").append("<a onclick=\"'. $toTopFunc .'\" style=\"position:fixed;z-index:999999;display:block;text-align:center;cursor:pointer;text-decoration:none;'. $addToTopCustom .'\">'. $addToTopLabel .'</a>");';
+    };
 
-	$html .= '
+    // PREPARE EXTENDED SAVE-BUTTON
+    if( $extSaveButtons != 'Disabled' ) {
+	global $_style, $_lang;
+	$jDocReady[] = '
+			$j("#stay").hide();
+			$j("#stay").val(2);
+			$j("#Button1").attr("title","'. $_lang["save"] .' + '. $_lang["stay"] .'");
+			$j("#Button1").append("<a style=\"cursor: pointer;padding-left:1em;padding-right:1em;\" onclick=\"$j(\'#stay\').val(\'1\'); documentDirty=false; document.mutate.save.click();saveWait(\'mutate\');\" title=\"'. $_lang["save"] .' + '. $_lang["stay_new"] .'\"><img src=\"'. $_style["icons_new_document"] .'\" /></a>");
+			$j("#Button1").append("<a style=\"cursor: pointer;padding-left:1em;padding-right:1em;margin-right:1em;\" onclick=\"$j(\'#stay\').val(\'\'); documentDirty=false; document.mutate.save.click();saveWait(\'mutate\');\" title=\"'. $_lang["save"] .' + '. $_lang["close"] .'\"><img src=\"'. $_style["icons_cancel"] .'\" /></a>");
+';
+    };
+
+    // FIX JUMP TO TOP
+    if( $scrollFix != 'Disabled' || $modx11 ) {
+	$jWinLoad[] = '$j("#Button1 > a").removeAttr("href").css("cursor","pointer");';
+    };
+
+    $html .= '
 		<script>
-			'. $scrollFixFunctions . $tabHeightFunctions .'
+			'. $scrollFixFunctions . $tabHeightFunctions;
+    $html .= !empty( $jDocReady) ? '
 			$j(document).ready(function() {
 				'. implode("\n				", $jDocReady ) .'
-			});
+			});' : '';
+    $html .= !empty( $jWinLoad) ? '
 			$j(window).load(function() {
 				'. implode("\n				", $jWinLoad ) .'
-			});
+			});' : '';
+    $html .= !empty( $jWinUnLoad) ? '
 			$j(window).unload(function() {
 				'. implode("\n				", $jWinUnLoad ) .'
-			});
+			});' : '';
+    $html .= !empty( $jWinResize) ? '
 			$j(window).resize(function() {
 				'. implode("\n				", $jWinResize ) .'
-			});
-
+			});' : '';
+    $html .= '
 		</script>';
 
-	$html .= '
+    $html .= '
 	<!-- ScrollFix End -->
 ';
 
-	$e->output($html);
+    $e->output($html);
 }
